d45c241b0aaf52be9b79743f2afb4e9c2757c6ca
Nawel <naweltv@gmail.com>
2025-11-04 17:26:03 -0400

    feat(auth) zod validation function done

--- src/api/auth/auth.validation.ts
+++ src/api/auth/auth.validation.ts
@@ -8,3 +8,8 @@ export const registerSchema = z.object({
         last_name: z.string().optional()
 
 })
+
+export const loginSchema = z.object({
+        email: z.email('Invalid email address'),
+        password: z.string().min(6, 'Password must be at least 6 characters long')
+})
