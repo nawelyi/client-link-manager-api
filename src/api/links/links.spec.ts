a54172cc90c4824e445c89146168432ce9cd3030
Nawel <naweltv@gmail.com>
2025-11-12 21:50:27 -0400

    feat(link) link test done

---
+++ src/api/links/links.spec.ts
@@ -0,0 +1,55 @@
+
+import Request  from "supertest";
+import app from  '../../app' 
+import prisma from "../../core/prisma";
+import { Response } from "supertest";
+
+
+const request = Request(app);
+
+
+describe('LINKS API', () => {
+    let getClient: Response;
+    let token: Response;
+        beforeAll(async () => {
+            const registerUser = {
+                first_name: 'John',
+                last_name: 'Doe',
+                email: 'naweltv@gmail.com',
+                password: 'password123'
+            }
+            await request.post('/api/v1/auth/register').send(registerUser);
+            token = await request.post('/api/v1/auth/login').send({email: registerUser.email, password: registerUser.password});
+            const createClient = {
+                name: 'Test Client',
+            }
+             getClient = await request.post('/api/v1/client').set('Authorization', `Bearer ${token.body.token}`).send(createClient);
+
+            
+
+        });
+        afterAll(async () => {
+            await prisma.link.deleteMany({});
+            await prisma.client.deleteMany({});
+            await prisma.user.deleteMany({});
+        })
+
+    describe('POST /api/v1/links', () => {
+      
+        it('should create a new link', async () => {
+            const body = {
+                title: 'Test Link',
+                url: 'https://example.com',
+                clientId: getClient.body.id 
+
+            }
+            const response = await request.post('/api/v1/links').set('Authorization', `Bearer ${token.body.token}`).send(body);
+
+            expect(response.status).toBe(201);
+            expect(response.body).toHaveProperty('id');
+            expect(response.body.title).toBe(body.title);
+            expect(response.body.url).toBe(body.url);
+            expect(response.body.clientId).toBe(body.clientId);
+        });
+    });
+});
